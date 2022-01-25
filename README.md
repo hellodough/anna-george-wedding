577201708a2be41400e5151fd95548d85263167a
Anna <doannado@gmail.com>
2022-01-25 09:40:38 -0600

    feat: deployment branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# ado-wedding
- 
--- public/build/routes/admin/edit/$slug-ZXSILQQ6.js
+++
@@ -1,7 +0,0 @@
-import {
-  init_react
-} from "/build/_shared/chunk-E7VMOUYL.js";
-
-// browser-route-module:/Users/ado/Documents/GitHub/anna-george-wedding/ado-wedding/app/routes/admin/edit/$slug.tsx?browser
-init_react();
-//# sourceMappingURL=/build/routes/admin/edit/$slug-ZXSILQQ6.js.map

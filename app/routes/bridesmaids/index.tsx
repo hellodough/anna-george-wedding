d2811ed00989a5ab3157af62f9bf06fe5de35b51
Anna <doannado@gmail.com>
2022-05-14 21:09:26 -0500

    feat: add groomsmen pg, update bridesmaids links

--- app/routes/bridesmaids/index.tsx
+++ app/routes/bridesmaids/index.tsx
@@ -1,21 +1,23 @@
 import { HeaderNav } from "~/components/HeaderNav/HeaderNav";
 
-
 export default function Bridesmaids() {
   const headerProps = {
     currentPage: "bridesmaids",
-    inverted: false
-  }
+    inverted: false,
+  };
   return (
     <div className="bg-amber-400 h-screen bg-cover">
       <HeaderNav {...headerProps} />
-      <div className="lg:px-20 sm:px-4">
+      <div className="lg:px-20 sm:px-4 sm:pt-24 pt-12 ">
         <div className="grid xs:gap-1 gap-4 xs:grid-cols-1 grid-cols-2 s:grid-cols-4">
-          <div className="flex items-baseline items-stretch">
-            <img className="w-1/2" src="https://github.com/hellodough/anna-george-wedding/blob/e4e91e46c49ae9ddb7f87874d4d99c59e657b686/ado-wedding/assets/images/smile-cutout.png?raw=true" />
+          <div className="sm:flex items-baseline items-stretch justify-center hidden">
+            <img
+              className="max-w-md"
+              src="https://github.com/hellodough/anna-george-wedding/blob/e4e91e46c49ae9ddb7f87874d4d99c59e657b686/ado-wedding/assets/images/smile-cutout.png?raw=true"
+            />
           </div>
           {/* welcome text */}
-          <div className="flex items-center s:col-span-3">
+          <div className="flex items-center col-span-3 sm:col-span-1 px-4">
             <div>
               <div className="text-4xl xs:my-4 my-8 font-display">Hey,</div>
               <p className="lg:text-lg xs:text-sm mb-6 font-body text-base max-w-sm">
@@ -23,8 +25,23 @@ export default function Bridesmaids() {
                 celebrate with you. I created this page so we can plan things
                 out, and we can use "discord" for chatting.
               </p>
-              <a href="https://discord.gg/UGqPPgnPNH" className="btn-primary">
+              <a
+                href="https://discord.gg/UGqPPgnPNH"
+                className="btn-primary mb-4 block text-center"
+              >
                 Start talking
+              </a>
+              <a
+                href="https://doannado.notion.site/Bridesmaid-clothing-5601787618d2487ea3c8da2f0851c6f8"
+                className="btn-secondary my-4 block text-center"
+              >
+                Bridesmaids Attire
+              </a>
+              <a
+                href="https://doannado.notion.site/Bachelorette-Party-fd45c58fccae4619b986d2ed7a7128e1"
+                className="btn-secondary my-4 block text-center"
+              >
+                Bachlorette party
               </a>
             </div>
           </div>
---
+++ app/routes/groomsmen/index.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "remix";
+export async function loader({ request }) {
+    return redirect("https://doannado.notion.site/Groomsmen-clothes-40bf956158304373893d821d9274488f")
+  }
+
+export default function Groomsmen() {
+    
+    return null;
+}
